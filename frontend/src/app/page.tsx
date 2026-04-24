dde43530836495d3bad3bd6bfb1ebc2054766d7a
Soham Deogaonkar <sohamdeogaonkar429@gmail.com>
2026-04-24 10:32:20 +0000

    Update Image

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,25 +1,29 @@
 import { PageContainer } from "@/components/common";
 import Image from "next/image";
 
+import lightImg from "@/images/KreedaHiringBot_Light.jpg";
+import darkImg from "@/images/KreedaHiringBot_Dark.jpg";
+
 export default function DashboardPage() {
   return (
     <PageContainer className="relative min-h-[calc(100vh-4rem)] w-full max-w-none px-0">
       <div className="relative w-full h-[calc(100vh-4rem)]">
         <Image
-          src="/KreedaHiringBot_Light.jpg"
+          src={lightImg}
           alt="Kreeda Hiring Bot"
           fill
           priority
+          placeholder="blur"
           className="object-cover object-bottom block dark:hidden"
         />
         <Image
-          src="/KreedaHiringBot_Dark.jpg"
+          src={darkImg}
           alt="Kreeda Hiring Bot"
           fill
           priority
+          placeholder="blur"
           className="object-cover object-bottom hidden dark:block"
         />
-      
       </div>
     </PageContainer>
   );
